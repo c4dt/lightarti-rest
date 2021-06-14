44df1affa41eba305ec3ae0fb0998c00d1295e9d
Linus Gasser <linus.gasser@epfl.ch>
2021-06-14 16:51:38 +0200

    Adding Get-tests

--- src/ffi/ios.rs
+++ src/ffi/ios.rs
@@ -3,13 +3,25 @@ use core_foundation::{
     base::TCFType,
     string::{CFString, CFStringRef},
 };
-use crate::arti::tls_get;
+use crate::{Request, DirectoryCache};
+use log::LevelFilter;
 
 #[no_mangle]
 pub unsafe extern "C" fn call_tls_get(domain_cc: *const c_char) -> CFStringRef {
+    simple_logging::log_to_stderr(LevelFilter::Debug);
     let domain = cstring_to_str(&domain_cc);
-    match tls_get(domain, None){
-        Ok(s) => to_cf_str(format!("Result is: {}", s)),
+    let req = &Request{
+        method: "GET".to_string(),
+        url: format!("https://{}/index.html", domain),
+        headers: Default::default(),
+        body: None
+    };
+    match req.send(&DirectoryCache{
+        tmp_dir: None,
+        nodes: None,
+        relays: None
+    }){
+        Ok(s) => to_cf_str(format!("Result is: {:?}", s.body.unwrap())),
         Err(e) => to_cf_str(format!("Error while getting result: {}", e)),
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
 use anyhow::{Result, bail};
-use log::{info};
+use log::info;
+use std::collections::HashMap;
+use url::Url;
+
+use crate::arti::tls_send;
 
 mod arti;
 mod ffi;
@@ -8,56 +12,92 @@ mod ffi;
 /// the url and the parameters passed to the remote end.
 /// Only http and https URLs are supported for the moment.
 pub struct Request {
+    pub method: String,
     pub url: String,
-    pub user_agent: Option<String>,
+    pub headers: HashMap<String, String>,
     pub body: Option<String>,
 }
+
+impl Request {
+    /// Sends the request to the given URL. It returns the response.
+    pub fn send(&self, dir_cache: &DirectoryCache) -> Result<Response> {
+        let request = &self.to_str()?;
+        info!("Contacting path {}", request);
+        let resp = tls_send(&self.host()?, request, dir_cache)?;
+        Ok(Response {
+            code: 100,
+            body: Some(resp),
+        })
+    }
+
+    /// Returns the string to be sent to the host.
+    fn to_str(&self) -> Result<String> {
+        info!("Sending GET request to {}", self.url);
+        let url = Url::parse(&self.url)?;
+        if url.scheme() != "https" {
+            bail!("Currently only supports https");
+        }
+        let host = &url.domain().unwrap();
+        // TODO: add other headers
+        // TODO: add body
+        // Needs to follow https://www.rfc-editor.org/rfc/rfc7230.html#page-19
+        Ok(format!("{} {} HTTP/1.0\r\nHost: {}\r\n\r\n",
+                   self.method, url.path(), host))
+    }
+
+    /// Returns the host, or an error if it's not defined.
+    fn host(&self) -> Result<String> {
+        let url = Url::parse(&self.url)?;
+        Ok(url.domain().unwrap().into())
+    }
+}
+
+/// The DirectoryCache allows arti to avoid having to download all the nodes and
+/// relays when starting up. This improves the request-time a lot, as arti now only
+/// needs to set up the circuit, and not download the information of all the nodes.
+/// Even if some nodes are not available anymore, this is not a problem.
+///
+/// If tmp_dir is set, it is used to store temporary files during the setup. This is only
+/// needed in Android.
+pub struct DirectoryCache {
+    pub tmp_dir: Option<String>,
+    pub nodes: Option<String>,
+    pub relays: Option<String>,
+}
+
 
 /// Response from the REST call. If an error happened during the call,
 /// no Response will be sent.
 /// The 'code' is parsed from the response.
+#[derive(Debug)]
 pub struct Response {
     pub code: i32,
     pub body: Option<String>,
 }
 
-/// Sends a GET request to the given URL. It returns the response.
-pub fn send_get(req: Request) -> Result<Response> {
-    info!("Sending GET request to {}", req.url);
-    bail!("Not yet implemented")
-}
-
-/// Sends a POST request to the given URL. It returns the response.
-pub fn send_post(req: Request) -> Result<Response> {
-    info!("Sending POST request to {}", req.url);
-    bail!("Not yet implemented")
-}
-
-/// Sends a PUT request to the given URL. It returns the response.
-pub fn send_put(req: Request) -> Result<Response> {
-    info!("Sending PUT request to {}", req.url);
-    bail!("Not yet implemented")
-}
-
-/// Sends a DELETE request to the given URL. It returns the response.
-pub fn send_delete(req: Request) -> Result<Response> {
-    info!("Sending DELETE request to {}", req.url);
-    bail!("Not yet implemented")
-}
-
 #[test]
 fn test_get() {
-    match send_get(Request{
+    use log::LevelFilter;
+
+    simple_logging::log_to_stderr(LevelFilter::Debug);
+
+    let req = Request {
+        method: "GET".into(),
         url: "https://www.c4dt.org/index.html".into(),
-        user_agent: None,
+        headers: HashMap::new(),
         body: None,
-    }){
+    };
+    match req.send(&DirectoryCache {
+        tmp_dir: None,
+        nodes: None,
+        relays: None,
+    }) {
         Ok(resp) => {
             info!("Sent GET to c4dt.org and got code {}", resp.code);
             if let Some(body) = resp.body {
                 info!("Body is: {}", body);
             }
-        },
+        }
         Err(e) => {
             panic!("Encountered error: {}", e);
         }
