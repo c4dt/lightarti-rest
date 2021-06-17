fd6a394feaddad9b459fb2be6a389d4c214c4271
tharvik <tharvik@users.noreply.github.com>
2021-06-17 14:42:01 +0200

    ffi: use new Client interface

--- src/ffi/android.rs
+++ src/ffi/android.rs
@@ -1,10 +1,10 @@
-use jni::JNIEnv;
 use jni::objects::{JClass, JString};
 use jni::sys::jstring;
+use jni::JNIEnv;
 use log::info;
 
-use crate::{Request, DirectoryCache};
-
+use crate::{client::Client, DirectoryCache};
+use http::Request;
 
 const ANDROID_LOG_TAG: &str = "ArtiLib";
 
@@ -14,20 +14,20 @@ pub unsafe extern "system" fn Java_org_c4dt_artiwrapper_JniApi_hello(
     _: JClass,
     who: JString,
 ) -> jstring {
-    let str: String = env.get_string(who)
-        .expect("Couldn't create rust string").into();
+    let str: String = env
+        .get_string(who)
+        .expect("Couldn't create rust string")
+        .into();
 
-    let output = env.new_string(format!("Hello {}!", str))
+    let output = env
+        .new_string(format!("Hello {}!", str))
         .expect("Couldn't create java string");
 
     output.into_inner()
 }
 
 #[no_mangle]
-pub unsafe extern "system" fn Java_org_c4dt_artiwrapper_JniApi_initLogger(
-    _: JNIEnv,
-    _: JClass,
-) {
+pub unsafe extern "system" fn Java_org_c4dt_artiwrapper_JniApi_initLogger(_: JNIEnv, _: JClass) {
     // Important: Logcat doesn't contain stdout / stderr so we need a custom logger.
     // An alternative solution to android_logger, is to register a callback
     // (Using the same functionality as registerCallback) to send the logs.
@@ -50,21 +50,35 @@ pub unsafe extern "system" fn Java_org_c4dt_artiwrapper_JniApi_tlsGet(
     cache_dir_j: JString,
     domain_j: JString,
 ) -> jstring {
-    let cache_dir: String = env.get_string(cache_dir_j).expect("Couldn't create rust string").into();
-    let domain: String = env.get_string(domain_j).expect("Couldn't create rust string").into();
-    let req = &Request{
-        method: "GET".to_string(),
-        url: format!("https://{}/index.html", domain),
-        headers: Default::default(),
-        body: None
-    };
-    let output = match req.send(&DirectoryCache{
+    let cache_dir: String = env
+        .get_string(cache_dir_j)
+        .expect("Couldn't create rust string")
+        .into();
+    let domain: String = env
+        .get_string(domain_j)
+        .expect("Couldn't create rust string")
+        .into();
+
+    let req = Request::get(format!("https://{}", domain))
+        .header("Host", domain)
+        .body(vec![])
+        .expect("create request");
+
+    let client = Client::new(DirectoryCache {
         tmp_dir: Some(cache_dir),
         nodes: None,
-        relays: None
-    }) {
-        Ok(s) => format!("Result is: {:?}", s),
+        relays: None,
+    });
+
+    let output = match client.send(req) {
+        Ok(s) => format!(
+            "Result is: {:?}",
+            s.map(|raw| String::from_utf8(raw).expect("decode body as utf8"))
+        ),
         Err(e) => format!("Error while getting result: {}", e),
     };
-    env.new_string(output).expect("Failed to build java string").into_inner()
+
+    env.new_string(output)
+        .expect("Failed to build java string")
+        .into_inner()
 }
