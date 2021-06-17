7311e373208dfe73610707c2d759981e96cff638
tharvik <tharvik@users.noreply.github.com>
2021-06-17 13:07:48 +0200

    lib: rewrite and split
    Closes: #19
    Closes: #20

---
+++ src/client.rs
@@ -0,0 +1,121 @@
+use std::io::Write;
+
+use anyhow::{bail, Context, Result};
+use log::trace;
+
+use http::{Request, Response};
+
+use super::{arti::tls_send, DirectoryCache};
+
+pub struct Client {
+    dir_cache: DirectoryCache,
+}
+
+impl Client {
+    pub fn new(dir_cache: DirectoryCache) -> Self {
+        Self { dir_cache }
+    }
+
+    /// Sends the request to the given URL. It returns the response.
+    pub fn send(&self, req: Request<Vec<u8>>) -> Result<Response<Vec<u8>>> {
+        trace!("request: {:?}", req);
+
+        let uri = req.uri().clone();
+        let host = uri.host().context("no host found")?;
+
+        let raw_req = serialize_request(req).context("serialize request")?;
+
+        let raw_resp = tls_send(
+            host,
+            &String::from_utf8(raw_req).context("encode serialized as utf-8")?,
+            &self.dir_cache,
+        )?
+        .into_bytes();
+
+        let resp = deserialize_response(raw_resp);
+
+        trace!("response: {:?}", resp);
+
+        resp
+    }
+}
+
+fn serialize_request(req: Request<Vec<u8>>) -> Result<Vec<u8>> {
+    let (parts, mut body) = req.into_parts();
+
+    let mut ret = Vec::new();
+
+    write!(
+        &mut ret,
+        "{} {} {:?}\r\n",
+        parts.method, parts.uri, parts.version,
+    )
+    .context("write status line")?;
+
+    for (key, value) in parts.headers {
+        write!(
+            &mut ret,
+            "{}: {}\r\n",
+            key.context("missing header name")?,
+            value.to_str().context("serialize header value as string")?,
+        )
+        .context("write header")?;
+    }
+
+    write!(&mut ret, "\r\n").context("write last EOL")?;
+
+    ret.append(&mut body);
+
+    Ok(ret)
+}
+
+fn deserialize_response(mut raw_resp: Vec<u8>) -> Result<Response<Vec<u8>>> {
+    const MAX_HEADERS: usize = 16;
+
+    let mut headers = [httparse::EMPTY_HEADER; MAX_HEADERS];
+
+    let mut http_resp = httparse::Response::new(&mut headers);
+    let parsed = http_resp.parse(raw_resp.as_slice())?;
+    if parsed.is_partial() {
+        bail!("unfinished response");
+    }
+
+    let mut builder = Response::builder()
+        .status(http_resp.code.context("no status")?)
+        .version(if http_resp.version.context("no version")? == 0 {
+            http::Version::HTTP_10
+        } else {
+            http::Version::HTTP_11
+        });
+    for header in http_resp.headers {
+        builder = builder.header(header.name, header.value)
+    }
+    builder
+        .body(raw_resp.split_off(parsed.unwrap()))
+        .context("create response")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get() {
+        simple_logging::log_to(std::io::stdout(), log::LevelFilter::Debug);
+
+        let resp = Client::new(DirectoryCache {
+            tmp_dir: None,
+            nodes: None,
+            relays: None,
+        })
+        .send(
+            Request::get("https://www.c4dt.org")
+                .header("Host", "www.c4dt.org")
+                .body(vec![])
+                .expect("create get request"),
+        )
+        .expect("send request");
+
+        assert_eq!(resp.status(), 200);
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,56 +1,7 @@
-use anyhow::{Result, bail};
-use log::info;
-use std::collections::HashMap;
-use url::Url;
-
-use crate::arti::tls_send;
+pub mod client;
 
 mod arti;
 mod ffi;
-
-/// Request for the different REST methods. Here we don't differentiate between
-/// the url and the parameters passed to the remote end.
-/// Only http and https URLs are supported for the moment.
-pub struct Request {
-    pub method: String,
-    pub url: String,
-    pub headers: HashMap<String, String>,
-    pub body: Option<String>,
-}
-
-impl Request {
-    /// Sends the request to the given URL. It returns the response.
-    pub fn send(&self, dir_cache: &DirectoryCache) -> Result<Response> {
-        let request = &self.to_str()?;
-        info!("Contacting path {}", request);
-        let resp = tls_send(&self.host()?, request, dir_cache)?;
-        Ok(Response {
-            code: 100,
-            body: Some(resp),
-        })
-    }
-
-    /// Returns the string to be sent to the host.
-    fn to_str(&self) -> Result<String> {
-        info!("Sending GET request to {}", self.url);
-        let url = Url::parse(&self.url)?;
-        if url.scheme() != "https" {
-            bail!("Currently only supports https");
-        }
-        let host = &url.domain().unwrap();
-        // TODO: add other headers
-        // TODO: add body
-        // Needs to follow https://www.rfc-editor.org/rfc/rfc7230.html#page-19
-        Ok(format!("{} {} HTTP/1.0\r\nHost: {}\r\n\r\n",
-                   self.method, url.path(), host))
-    }
-
-    /// Returns the host, or an error if it's not defined.
-    fn host(&self) -> Result<String> {
-        let url = Url::parse(&self.url)?;
-        Ok(url.domain().unwrap().into())
-    }
-}
 
 /// The DirectoryCache allows arti to avoid having to download all the nodes and
 /// relays when starting up. This improves the request-time a lot, as arti now only
@@ -63,43 +14,4 @@ pub struct DirectoryCache {
     pub tmp_dir: Option<String>,
     pub nodes: Option<String>,
     pub relays: Option<String>,
-}
-
-
-/// Response from the REST call. If an error happened during the call,
-/// no Response will be sent.
-/// The 'code' is parsed from the response.
-#[derive(Debug)]
-pub struct Response {
-    pub code: i32,
-    pub body: Option<String>,
-}
-
-#[test]
-fn test_get() {
-    use log::LevelFilter;
-
-    simple_logging::log_to_stderr(LevelFilter::Debug);
-
-    let req = Request {
-        method: "GET".into(),
-        url: "https://www.c4dt.org/index.html".into(),
-        headers: HashMap::new(),
-        body: None,
-    };
-    match req.send(&DirectoryCache {
-        tmp_dir: None,
-        nodes: None,
-        relays: None,
-    }) {
-        Ok(resp) => {
-            info!("Sent GET to c4dt.org and got code {}", resp.code);
-            if let Some(body) = resp.body {
-                info!("Body is: {}", body);
-            }
-        }
-        Err(e) => {
-            panic!("Encountered error: {}", e);
-        }
-    }
 }
