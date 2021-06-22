76815830310f4bfc3c1cbcbe1478bb627bade2fa
tharvik <tharvik@users.noreply.github.com>
2021-06-22 17:18:37 +0200

    fixup! drop magic config

--- src/arti.rs
+++ src/arti.rs
@@ -4,46 +4,24 @@ use crate::DirectoryCache;
 /// This is a simple wrapper around arti to offer a synchronous
 /// REST interface to mobile libraries.
 use anyhow::{anyhow, Context, Result};
-use serde::Deserialize;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio_rustls::{rustls::ClientConfig, webpki::DNSNameRef, TlsConnector};
 use tor_client::TorClient;
-use tor_config::CfgPath;
 #[cfg(not(target_os = "android"))]
-use tor_dirmgr::NetDirConfig;
-use tor_dirmgr::{DownloadScheduleConfig, NetworkConfig};
 use tor_rtcompat::{Runtime, SpawnBlocking};
 use tracing::{debug, info, trace};
 
 mod conv;
 
-/// Some structures that were defined in the arti main source and that I took over
-/// without thinking...
-
-const ARTI_DEFAULTS: &str = concat!(
-    include_str!("./arti_defaults.toml"),
-    include_str!("./authorities.toml"),
-);
-
 /// This connection sends a generic request over TLS to the host.
 /// It returns the result from the request, or an error.
 pub fn tls_send(host: &str, request: &str, dir_cache: &DirectoryCache) -> Result<String> {
     info!("Starting TorClient");
-    let mut cfg = config::Config::new();
-    cfg.merge(config::File::from_str(
-        ARTI_DEFAULTS,
-        config::FileFormat::Toml,
-    ))?;
-
-    debug!("Load config");
-    let empty: Vec<String> = vec![];
-    tor_config::load(&mut cfg, tor_config::default_config_file(), &empty, &empty)?;
-    let config: ArtiConfig = cfg.try_into()?;
 
     let runtime = tor_rtcompat::create_runtime()?;
     runtime.block_on(async {
         debug!("Getting tor connection");
-        let tor = get_tor(runtime.clone(), config, dir_cache.tmp_dir.as_deref())
+        let tor = get_tor(runtime.clone(), dir_cache.tmp_dir.as_deref())
             .await
             .context("get tor")?;
 
@@ -85,11 +63,7 @@ async fn get_result(tor: TorClient<impl Runtime>, host: &str, request: &str) -> 
 
 // On iOS, the get_dir_config works as supposed, so no need to do special treatment.
 #[cfg(not(target_os = "android"))]
-async fn get_tor<T: Runtime>(
-    runtime: T,
-    config: ArtiConfig,
-    cache_dir: Option<&str>,
-) -> Result<TorClient<T>> {
+async fn get_tor<T: Runtime>(runtime: T, cache_dir: Option<&str>) -> Result<TorClient<T>> {
     let cache_path = std::path::Path::new(cache_dir.unwrap());
     debug!(?cache_path);
 
@@ -106,11 +80,7 @@ async fn get_tor<T: Runtime>(
 // For Android, the cache path needs to be set, so the whole config needs to be initialized.
 // This could of course be cleaned up...
 #[cfg(target_os = "android")]
-async fn get_tor<T: Runtime>(
-    runtime: T,
-    config: ArtiConfig,
-    cache_dir: Option<&str>,
-) -> Result<TorClient<T>> {
+async fn get_tor<T: Runtime>(runtime: T, cache_dir: Option<&str>) -> Result<TorClient<T>> {
     use std::path::Path;
 
     debug!("New dircfg");
@@ -135,56 +105,6 @@ async fn get_tor<T: Runtime>(
 
     debug!("Connect to tor");
     TorClient::bootstrap(runtime, netdircfg).await
-}
-
-/// Configuration for where information should be stored on disk.
-///
-/// This section is for read/write storage
-#[derive(Deserialize, Debug, Clone)]
-#[serde(deny_unknown_fields)]
-pub struct StorageConfig {
-    /// Location on disk for cached directory information
-    cache_dir: CfgPath,
-    /// Location on disk for less-sensitive persistent state information.
-    #[allow(unused)]
-    state_dir: CfgPath,
-}
-
-/// Structure to hold our configuration options, whether from a
-/// configuration file or the command line.
-///
-/// NOTE: These are NOT the final options or their final layout.
-/// Expect NO stability here.
-#[derive(Deserialize, Debug, Clone)]
-#[serde(deny_unknown_fields)]
-pub struct ArtiConfig {
-    /// Port to listen on (at localhost) for incoming SOCKS
-    /// connections.
-    socks_port: Option<u16>,
-    /// Whether to log at trace level.
-    trace: bool,
-
-    /// Information about the Tor network we want to connect to.
-    network: NetworkConfig,
-
-    /// Directories for storing information on disk
-    storage: StorageConfig,
-
-    /// Information about when and how often to download directory information
-    download_schedule: DownloadScheduleConfig,
-}
-
-#[cfg(not(target_os = "android"))]
-impl ArtiConfig {
-    fn get_dir_config(&self) -> Result<NetDirConfig> {
-        let mut dircfg = tor_dirmgr::NetDirConfigBuilder::new();
-        dircfg.set_network_config(self.network.clone());
-        dircfg.set_timing_config(self.download_schedule.clone());
-        dircfg
-            .use_default_cache_path()
-            .context("default cache path")?;
-        dircfg.finalize().context("finalize")
-    }
 }
 
 #[cfg(test)]
