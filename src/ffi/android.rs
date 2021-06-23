6388d18deb14e4c210572cac9844611becbf866b
Christian Grigis <christian.grigis@epfl.ch>
2021-06-23 09:54:03 +0200

    First version of tlsPost()

--- src/ffi/android.rs
+++ src/ffi/android.rs
@@ -1,8 +1,8 @@
-use http::Request;
-use jni::objects::{JClass, JString};
-use jni::sys::jstring;
+use http::{Request, Version};
+use jni::objects::{JClass, JString, JObject, JList, JMap, JValue};
+use jni::sys::{jstring, jbyteArray, jint, jobject};
 use jni::JNIEnv;
-use tracing::{info, log::Level};
+use tracing::{info, trace, log::Level};
 
 use crate::{client::Client, DirectoryCache};
 
@@ -80,6 +80,166 @@ pub unsafe extern "system" fn Java_org_c4dt_artiwrapper_JniApi_tlsGet(
     };
 
     env.new_string(output)
-        .expect("Failed to build java string")
+        .expect("build java string")
+        .into_inner()
+}
+
+#[no_mangle]
+pub unsafe extern "system" fn Java_org_c4dt_artiwrapper_JniApi_tlsPost(
+    env: JNIEnv,
+    _: JClass,
+    cache_dir_j: JString,
+    url_j: JString,
+    headers_j: JObject,
+    body_j: jbyteArray
+) -> jobject {
+    let cache_dir: String = env
+        .get_string(cache_dir_j)
+        .expect("create rust string for `cache_dir_j`")
+        .into();
+    let url: String = env
+        .get_string(url_j)
+        .expect("create rust string for `url_j`")
+        .into();
+    let body: Vec<u8> = env
+        .convert_byte_array(body_j)
+        .expect("create byte array");
+
+    let mut req_builder = Request::post(format!("{}", url))
+        .version(Version::HTTP_10);
+
+    let headers_jmap: JMap = env
+        .get_map(headers_j)
+        .expect("create JMap");
+
+    for (key, value_list) in headers_jmap
+        .iter()
+        .expect("create JMap iterator") {
+        let header_name: String = env
+            .get_string(JString::from(key))
+            .expect("create rust string for header name")
+            .into();
+        trace!("Request header_name: {:?}", header_name);
+
+        let header_value_list: JList = env
+            .get_list(value_list)
+            .expect("create JList");
+
+        for value in header_value_list
+            .iter()
+            .expect("create JList iterator") {
+            let header_value: String = env
+                .get_string(JString::from(value))
+                .expect("create rust string for header value")
+                .into();
+            trace!("    Request header_value: {:?}", header_value);
+
+            req_builder = req_builder.header(header_name.as_str(), header_value);
+        }
+    }
+
+    let request = req_builder.body(body).expect("create request");
+    info!("Request: {:?}", request);
+
+    let client = Client::new(DirectoryCache {
+        tmp_dir: Some(cache_dir),
+        nodes: None,
+        relays: None,
+    });
+
+    let response = match client.send(request) {
+        Ok(s) => s,
+        Err(e) => {
+            let _ = env.throw(format!("Error while getting result: {}", e));
+            return JObject::null().into_inner();
+        },
+    };
+    info!("Response: {:?}", response);
+
+    let status: jint = response.status().as_u16().into();
+
+    let version = env
+        .new_string(match response.version() {
+            http::Version::HTTP_09 => "HTTP/0.9",
+            http::Version::HTTP_10 => "HTTP/1.0",
+            http::Version::HTTP_11 => "HTTP/1.1",
+            http::Version::HTTP_2 => "HTTP/2",
+            http::Version::HTTP_3 => "HTTP/3",
+            _ => "Unknown",
+        })
+        .expect("build http string version");
+
+    let headers = env.new_object(
+        env
+            .find_class("java/util/HashMap")
+            .expect("find java.util.HashMap"),
+        "()V",
+        &[],
+        ).expect("create HashMap");
+
+    for (key, value) in response.headers() {
+        trace!("Response header: {:?} → {:?}", key, value);
+
+        let mut entry = env.new_object(
+            env
+            .find_class("java/util/ArrayList")
+            .expect("find java.util.ArrayList"),
+            "()V",
+            &[],
+            ).expect("create ArrayList");
+
+        match env.call_method(
+            headers,
+            "putIfAbsent",
+            "(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;",
+            &[
+            env.new_string(key.as_str())
+                .expect("create JString for key")
+                .into(),
+            entry.into(),
+            ],
+            )
+            .expect("call HashMap.put()") {
+                JValue::Object(o) => {
+                    if o.into_inner() != std::ptr::null_mut() {
+                        trace!("Entry already existed -- appending");
+                        entry = o;
+                    }
+                },
+                _ => (),
+            }
+
+        env.call_method(
+            entry,
+            "add",
+            "(Ljava/lang/Object;)Z",
+            &[
+                env.new_string(
+                    value.to_str()
+                    .expect("convert header value to string")
+                    )
+                .expect("create JString for value").into()
+            ]
+            )
+            .expect("call List.add()");
+    }
+
+    let body = env
+        .byte_array_from_slice(response.body())
+        .expect("create byte array");
+
+    env.new_object(
+        env
+            .find_class("org/c4dt/artiwrapper/HttpResponse")
+            .expect("find HttpResult class"),
+        "(ILjava/lang/String;Ljava/util/Map;[B)V",
+        &[
+            status.into(),
+            version.into(),
+            headers.into(),
+            body.into(),
+        ]
+        )
+        .expect("create HttpResult")
         .into_inner()
 }
