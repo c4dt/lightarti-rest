e5d465fe53f77413b2ce19712c043253b71e9ff7
Laurent Girod <laurent.girod@epfl.ch>
2021-07-15 12:30:14 +0200

    Adapt tests to use generated authority

--- src/tests.rs
+++ src/tests.rs
@@ -18,25 +18,25 @@ pub fn setup_cache() -> TempDir {
     let tempdir = TempDir::new("tor-cache").expect("create temp dir");
 
     copy(
-        concat!(env!("CARGO_MANIFEST_DIR"), "/src/test-data/authority.txt"),
+        concat!(env!("CARGO_MANIFEST_DIR"), "/tools/directory-cache/authority.txt"),
         tempdir.path().join("authority.txt"),
     )
     .expect("copy temp authority file");
 
     copy(
-        concat!(env!("CARGO_MANIFEST_DIR"), "/src/test-data/certificate.txt"),
+        concat!(env!("CARGO_MANIFEST_DIR"), "/tools/directory-cache/certificate.txt"),
         tempdir.path().join("certificate.txt"),
     )
     .expect("copy temp certificate file");
 
     copy(
-        concat!(env!("CARGO_MANIFEST_DIR"), "/src/test-data/consensus.txt"),
+        concat!(env!("CARGO_MANIFEST_DIR"), "/tools/directory-cache/consensus.txt"),
         tempdir.path().join("consensus.txt"),
     )
     .expect("copy temp consensus file");
 
     copy(
-        concat!(env!("CARGO_MANIFEST_DIR"), "/src/test-data/microdescriptors.txt"),
+        concat!(env!("CARGO_MANIFEST_DIR"), "/tools/directory-cache/microdescriptors.txt"),
         tempdir.path().join("microdescriptors.txt"),
     )
     .expect("copy temp microdescriptors file");
