91fe1d8208d6883224c1eca610695a4315ff2b66
tharvik <tharvik@users.noreply.github.com>
2021-10-13 15:09:44 +0200

    from upstream to c4dt
    Closes: #71

--- src/lightarti/tor-netdoc/src/types.rs
+++
@@ -1,9 +0,0 @@
-//! Various types that can occur in parsed network documents.
-//!
-//! NOTE: Several of these modules may eventually move elsewhere,
-//! or become their own crates.
-
-pub mod family;
-pub(crate) mod misc;
-pub mod policy;
-pub mod version;
