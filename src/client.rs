46392eb1b9d4ede73e68161ebf4c02a08fb29ea8
tharvik <tharvik@users.noreply.github.com>
2022-02-28 15:20:50 +0100

    mv serialisation to http mod

--- src/client.rs
+++ src/client.rs
@@ -1,4 +1,4 @@
-use std::{io::Write, path::Path, sync::Arc};
+use std::{path::Path, sync::Arc};
 
 use anyhow::{bail, Context, Result};
 use arti_client::TorClient;
@@ -6,7 +6,10 @@ use http::{Request, Response};
 use tor_rtcompat::tokio::TokioNativeTlsRuntime;
 use tracing::trace;
 
-use crate::lightarti::{build_config, flatfiledirmgr::FlatFileDirMgrBuilder, send_request};
+use crate::{
+    http::{raw_to_response, request_to_raw},
+    lightarti::{build_config, flatfiledirmgr::FlatFileDirMgrBuilder, send_request},
+};
 
 type Runtime = TokioNativeTlsRuntime;
 
@@ -37,82 +40,16 @@ impl Client {
         let uri = req.uri().clone();
         let host = uri.host().context("no host found")?;
 
-        let raw_req = serialize_request(req).context("serialize request")?;
+        let raw_req = request_to_raw(req).context("serialize request")?;
         let raw_resp = send_request(&self.0, host, &raw_req)
             .await
             .context("tls send")?;
-        let resp = deserialize_response(raw_resp);
+        let resp = raw_to_response(raw_resp);
 
         trace!("response: {:?}", resp);
 
         resp
     }
-}
-
-fn serialize_request(req: Request<Vec<u8>>) -> Result<Vec<u8>> {
-    const EOL: &str = "\n";
-
-    let (parts, mut body) = req.into_parts();
-
-    let mut ret = Vec::new();
-
-    write!(
-        &mut ret,
-        "{} {} {:?}{}",
-        parts.method,
-        parts
-            .uri
-            .path_and_query()
-            .context("uri without path or query")?,
-        parts.version,
-        EOL,
-    )
-    .context("write status line")?;
-
-    for (key, value) in &parts.headers {
-        write!(
-            &mut ret,
-            "{}: {}{}",
-            key,
-            value.to_str().context("serialize header value as string")?,
-            EOL,
-        )
-        .context("write header")?;
-    }
-
-    write!(&mut ret, "{}", EOL).context("write last EOL")?;
-
-    ret.append(&mut body);
-
-    Ok(ret)
-}
-
-fn deserialize_response(mut raw_resp: Vec<u8>) -> Result<Response<Vec<u8>>> {
-    const MAX_HEADERS: usize = 16;
-
-    let mut headers = [httparse::EMPTY_HEADER; MAX_HEADERS];
-
-    let mut http_resp = httparse::Response::new(&mut headers);
-    let parsed = http_resp
-        .parse(raw_resp.as_slice())
-        .context("parse response")?;
-    if parsed.is_partial() {
-        bail!("unfinished response");
-    }
-
-    let mut builder = Response::builder()
-        .status(http_resp.code.context("no status")?)
-        .version(if http_resp.version.context("no version")? == 0 {
-            http::Version::HTTP_10
-        } else {
-            http::Version::HTTP_11
-        });
-    for header in http_resp.headers {
-        builder = builder.header(header.name, header.value)
-    }
-    builder
-        .body(raw_resp.split_off(parsed.unwrap()))
-        .context("create response")
 }
 
 #[cfg(test)]
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 pub mod client;
 
 mod ffi;
+mod http;
 mod lightarti;
 
 #[cfg(test)]
