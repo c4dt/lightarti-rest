c6a8a57983b69254db44a134895b2de4d70df5c0
Linus Gasser <linus.gasser@epfl.ch>
2022-08-26 17:48:14 +0200

    Running tests multiple times in case one of the nodes fails

--- tests/client.rs
+++ tests/client.rs
@@ -1,88 +1,99 @@
+use http::request::{Builder, Parts};
 use http::Request;
 use lightarti_rest::Client;
+use url::Url;
 
 mod utils;
 
+const MAX_TRIES: u32 = 4;
+
 #[tokio::test]
-pub async fn test_get() {
-    utils::setup_tracing();
-    let cache = utils::setup_cache();
-
-    let resp = Client::new(cache.path())
-        .await
-        .expect("create client")
-        .send(
-            Request::get("https://www.example.com")
-                .header("Host", "www.example.com")
-                .version(http::Version::HTTP_10)
-                .body(vec![])
-                .expect("create get request"),
-        )
-        .await
-        .expect("send request");
-
-    assert_eq!(resp.status(), 200);
+pub async fn test_get_short() {
+    test_get("https://www.example.com").await;
 }
 
 #[tokio::test]
 pub async fn test_get_long_response() {
-    utils::setup_tracing();
-    let cache = utils::setup_cache();
-
-    let resp = Client::new(cache.path())
-        .await
-        .expect("create client")
-        .send(
-            Request::get("https://www.admin.ch/gov/de/start.html")
-                .header("Host", "www.admin.ch")
-                .version(http::Version::HTTP_10)
-                .body(vec![])
-                .expect("create get request"),
-        )
-        .await
-        .expect("send request");
-
-    assert_eq!(resp.status(), 200);
+    test_get("https://www.admin.ch/gov/de/start.html").await;
 }
 
 #[tokio::test]
 pub async fn test_get_many_headers() {
-    utils::setup_tracing();
-    let cache = utils::setup_cache();
-
-    let resp = Client::new(cache.path())
-        .await
-        .expect("create client")
-        .send(
-            Request::get("https://www.sunrise.ch/en/home")
-                .header("Host", "www.sunrise.ch")
-                .version(http::Version::HTTP_10)
-                .body(vec![])
-                .expect("create get request"),
-        )
-        .await
-        .expect("send request");
-
-    assert_eq!(resp.status(), 200);
+    test_get("https://www.sunrise.ch/en/home").await;
 }
 
 #[tokio::test]
 pub async fn test_post() {
+    test_client(
+        Request::post("https://httpbin.org/post")
+            .header("Host", "httpbin.org")
+            .version(http::Version::HTTP_10)
+            .body(Vec::from("key1=val1&key2=val2"))
+            .expect("Couldn't build request"),
+    )
+    .await;
+}
+
+// Creates a GET request from an URI and calls test_client on it.
+async fn test_get(uri: &str) {
+    let url = Url::parse(uri).unwrap();
+
+    test_client(
+        Request::get(uri)
+            .header("Host", url.host_str().unwrap())
+            .version(http::Version::HTTP_10)
+            .body(vec![])
+            .map_err(|e| anyhow::anyhow!("Error in request: {}", e))
+            .expect("Couldn't build request"),
+    )
+    .await;
+}
+
+// Calls the lightarti-rest code up to MAX_TRIES to get a correct answer.
+// Returns on the first correct answer, or throws a panic if all MAX_TRIES failed.
+// This is necessary due to the sometimes erratic behaviour of the tor-nodes.
+async fn test_client(req: Request<Vec<u8>>) {
     utils::setup_tracing();
-    let cache = utils::setup_cache();
+    let (header, body) = req.into_parts();
+    let host = header.uri.clone();
 
-    let resp = Client::new(cache.path())
-        .await
-        .expect("create client")
-        .send(
-            Request::post("https://httpbin.org/post")
-                .header("Host", "httpbin.org")
-                .version(http::Version::HTTP_10)
-                .body(Vec::from("key1=val1&key2=val2"))
-                .expect("create post request"),
+    utils::test_n(MAX_TRIES, move || {
+        let request = clone_request(&header, &body);
+        Box::pin(async move {
+            let cache = utils::setup_cache();
+
+            let resp = Client::new(cache.path())
+                .await
+                .expect("create client")
+                .send(request)
+                .await;
+
+            match resp {
+                Ok(resp) => (resp.status() == 200)
+                    .then_some(())
+                    .ok_or_else(|| anyhow::anyhow!("Wrong status")),
+                Err(e) => Err(anyhow::anyhow!("Couldn't get response: {}", e)),
+            }
+        })
+    })
+    .await
+    .unwrap_or_else(|_| {
+        panic!(
+            "Didn't manage to pass in {} steps for domain {}",
+            MAX_TRIES, host
         )
-        .await
-        .expect("send request");
+    });
+}
 
-    assert_eq!(resp.status(), 200);
+fn clone_request(header: &Parts, body: &[u8]) -> Request<Vec<u8>> {
+    let mut builder = Builder::new()
+        .method(header.method.clone())
+        .uri(header.uri.clone())
+        .version(header.version);
+    let builder_header = builder.headers_mut().unwrap();
+    for header in header.headers.clone() {
+        builder_header.insert(header.0.unwrap(), header.1);
+    }
+
+    builder.body(body.to_vec()).unwrap()
 }
