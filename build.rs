c753024bf61268cac990903eaeb8e7d5147f4e45
Carine Dengler <git@carine-dengler.de>
2023-10-25 10:09:44 +0200

    fix: fix lint

--- build.rs
+++ build.rs
@@ -6,7 +6,9 @@ use std::env;
 fn main() {
     // NDK > 25 does not link to `libgcc` anymore
     // see https://github.com/p2panda/meli/pull/21/files
-    if env::var("CARGO_CFG_TARGET_ARCH").unwrap() == "x86_64" && env::var("CARGO_CFG_TARGET_OS").unwrap() == "android" {
+    if env::var("CARGO_CFG_TARGET_ARCH").unwrap() == "x86_64"
+        && env::var("CARGO_CFG_TARGET_OS").unwrap() == "android"
+    {
         let home: String = env::var("HOME").unwrap();
         println!("cargo:rustc-link-search={home}/Android/Sdk/ndk/25.2.9519653/toolchains/llvm/prebuilt/linux-x86_64/lib64/clang/14.0.7/lib/linux/");
         println!("cargo:rustc-link-lib=static=clang_rt.builtins-x86_64-android");
